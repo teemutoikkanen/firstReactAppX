ccb38c33e9850bba53a35212438325ce11402241
Toikkanen Teemu <teemu.toikkanen@aalto.fi>
2018-08-22 01:38:50 +0300

    etenee, bug: en saa extendedRange() importattuu

---
+++ src/asd.js
@@ -0,0 +1,142 @@
+'use strict'
+
+const {
+    raceCombos
+  , raceCombosForBoard
+  , raceRange
+  , raceRangeForBoard
+  , rates
+} = require('../../')
+const prange = require('prange')
+const { detailRange } = require('pdetail')
+const spok = require('spok')
+const ocat = require('./ocat')
+
+// 10,000 iterations seems minimum to get somewhat reliable results
+// Still we use maxDeviation to keep tests from failing too often
+const ITER = 1E4
+
+function arryifyCombo(combo) {
+  return [ combo[0] + combo[1], combo[2] + combo[3] ]
+}
+
+function expandRange(range) {
+  const rangeSetArray = prange(range).map(detailRange)
+  const rangeArray = []
+  rangeSetArray.forEach(x => Array.from(x).forEach(c => {
+    rangeArray.push(arryifyCombo(c))
+  }))
+  return rangeArray
+}
+
+function raceSingle(combo1, combo2, times) {
+  const arr1 = arryifyCombo(combo1)
+  const arr2 = arryifyCombo(combo2)
+  const res = raceCombos(arr1, arr2, times)
+  return rates(res)
+}
+
+function raceSingleForBoard(combo1, combo2, times, board) {
+  const arr1 = arryifyCombo(combo1)
+  const arr2 = arryifyCombo(combo2)
+  const res = raceCombosForBoard(arr1, arr2, times, board)
+  return rates(res)
+}
+
+function checkSingle(t, combo1, combo2, expectedWinRate, maxDeviation = 4, times = ITER) {
+  const { winRate: win, looseRate: loose, tieRate: tie } = raceSingle(combo1, combo2, times)
+  const msg = `${combo1} wins ${expectedWinRate}% vs.${combo2}, actual ${win}% vs ${loose}%, tie: ${tie}%`
+  const pass = expectedWinRate - maxDeviation < win && win < expectedWinRate + maxDeviation
+  t.ok(pass, msg)
+}
+
+function checkSingleForBoard(t, combo1, combo2, board, win, loose, tie, times = ITER) {
+  const res = raceSingleForBoard(combo1, combo2, ITER, board.split(' '))
+  if (win == null) {
+    ocat.log(res)
+    return
+  }
+  spok(t, res,
+    { $topic    : `${combo1} vs ${combo2} on ${board}`
+    , winRate   : spok.range(win[0], win[1])
+    , looseRate : spok.range(loose[0], loose[1])
+    , tieRate   : spok.range(tie[0], tie[1])
+    }
+  )
+}
+
+function checkSingleAll(t, combo1, combo2, expectedWinRate, maxDeviation = 4) {
+  const { winRate: win, looseRate: loose, tieRate: tie } = raceSingle(combo1, combo2, null)
+  const msg = `${combo1} wins ${expectedWinRate}% vs.${combo2}, actual ${win}% vs ${loose}%, tie: ${tie}%`
+  const pass = expectedWinRate - maxDeviation < win && win < expectedWinRate + maxDeviation
+  t.ok(pass, msg)
+}
+
+function checkSingleAllForBoard(t, combo1, combo2, board, win, loose, tie) {
+  const res = raceSingleForBoard(combo1, combo2, null, board.split(' '))
+  if (win == null) {
+    ocat.log(res)
+    return
+  }
+  spok(t, res,
+    { $topic    : `${combo1} vs ${combo2} on ${board}`
+    , winRate   : spok.range(win[0], win[1])
+    , looseRate : spok.range(loose[0], loose[1])
+    , tieRate   : spok.range(tie[0], tie[1])
+    }
+  )
+}
+
+function raceComboVsRange(combo, range, times, trackCombos) {
+  const arr = arryifyCombo(combo)
+  const expanded = expandRange(range)
+  return raceRange(arr, expanded, times, trackCombos)
+}
+
+function raceComboVsRangeForBoard(combo, range, times, trackCombos, board) {
+  const arr = arryifyCombo(combo)
+  const expanded = expandRange(range)
+  return raceRangeForBoard(arr, expanded, times, trackCombos, board)
+}
+
+function rateComboVsRange(combo, range, times, trackCombos) {
+  const res = raceComboVsRange(combo, range, times, trackCombos)
+  return rates(res)
+}
+
+function rateComboVsRangeForBoard(combo, range, times, trackCombos, board) {
+  const res = raceComboVsRangeForBoard(combo, range, times, trackCombos, board)
+  return rates(res)
+}
+
+function checkRange(t, combo, range, expectedWin, expectedLoose, maxDeviation = 4, times = ITER) {
+  const { winRate: win, looseRate: loose, tieRate: tie } = rateComboVsRange(combo, range, times)
+  const msg = `${combo} wins ${expectedWin}% and looses ${expectedLoose}% vs ${range}, actual ${win}% vs ${loose}%, tie: ${tie}`
+  const pass = expectedWin - maxDeviation < win && win < expectedWin + maxDeviation &&
+               expectedLoose - maxDeviation < loose && loose < expectedLoose + maxDeviation
+  t.ok(pass, msg)
+}
+
+function checkRangeAll(t, combo, range, expectedWin, expectedLoose, maxDeviation = 4) {
+  const { winRate: win, looseRate: loose, tieRate: tie } = rateComboVsRange(combo, range, null)
+  const msg = `${combo} wins ${expectedWin}% and looses ${expectedLoose}% vs ${range}, actual ${win}% vs ${loose}%, tie: ${tie}`
+  const pass = expectedWin - maxDeviation < win && win < expectedWin + maxDeviation &&
+               expectedLoose - maxDeviation < loose && loose < expectedLoose + maxDeviation
+  t.ok(pass, msg)
+}
+
+module.exports = {
+    checkSingle
+  , checkSingleForBoard
+  , checkSingleAll
+  , checkSingleAllForBoard
+  , checkRange
+  , checkRangeAll
+  , expandRange
+  , arryifyCombo
+  , raceComboVsRange
+  , rateComboVsRange
+  , raceComboVsRangeForBoard
+  , rateComboVsRangeForBoard
+  , raceSingleForBoard
+}
--- src/index.js
+++ src/index.js
@@ -1,62 +1,102 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-
+// import _ from './asd';
 
 const { raceRange, rates } = require('pec')
+const prange = require('prange')
 
 
-class P1RangeForm extends React.Component {
+class EquityCalculator extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {value: ''};
-
-    this.handleChange = this.handleChange.bind(this);
+    this.state = {
+      p1TextInput: '',
+      p2TextInput: '',
+      // more vars here ..
+    }
+    this.handleP1Input = this.handleP1Input.bind(this);
+    this.handleCalcButton = this.handleCalcButton.bind(this);
   }
 
-  handleChange(event) {
-    this.setState({value: event.target.value});
-    // alert('this.state.value: ' + this.state.value);
+  handleP1Input(event) {
+    this.setState({p1TextInput: event.target.value});
+    
   }
 
+  handleCalcButton() {
+    console.log(this.state.p1TextInput);
+    console.log(expandRange(this.state.p1TextInput))
+  }
 
+  // EquityCalculator render() function..
   render () {
+    // render stuff here..
     return (
-      <form>
-        <label>
-          P1 Range Text Input:
-          <input type='text' value={this.state.value} onChange={this.handleChange} />
-        </label>
-      </form>
-    )
+      <div className="equityCalculator">
+        <div className="p1TextInput">
+          <form>
+            <label>
+              P1 Range Text Input:
+              <input type='text' value={this.state.p1TextInput} onChange={this.handleP1Input} />
+            </label>
+          </form>
+        </div>
+        <div className ="calcButton">
+          <button onClick={() => this.handleCalcButton()}>Calculate</button>
+        </div>
+      </div>
+    );
   }
-  
 }
 
 
 
-const combo = [ 'Jh', 'Js' ]
-const range = [
-  [ 'Kh', 'Ks' ], [ 'Kh', 'Kd' ], [ 'Kh', 'Kc' ],
-  [ 'Ks', 'Kd' ], [ 'Ks', 'Kc' ], [ 'Kd', 'Kc' ],
-  [ 'Qh', 'Qs' ], [ 'Qh', 'Qd' ], [ 'Qh', 'Qc' ],
-  [ 'Qs', 'Qd' ], [ 'Qs', 'Qc' ], [ 'Qd', 'Qc' ]
-]
-
-const { win, loose, tie } = raceRange(combo, range, 1E4)
-const { winRate, looseRate, tieRate } = rates({ win, loose, tie })
-
-console.log('JJ performs as follows vs. [ KK, QQ ]')
-console.log('win: %d%% (%d times)', winRate, win)
-console.log('loose: %d%% (%d times)', looseRate, loose)
-console.log('tie: %d%% (%d times)', tieRate, tie)
 
 
 
+
+
+
+
+// PEC EXAMPLE
+// const combo = [ 'Jh', 'Js' ]
+// const range = [
+//   [ 'Kh', 'Ks' ], [ 'Kh', 'Kd' ], [ 'Kh', 'Kc' ],
+//   [ 'Ks', 'Kd' ], [ 'Ks', 'Kc' ], [ 'Kd', 'Kc' ],
+//   [ 'Qh', 'Qs' ], [ 'Qh', 'Qd' ], [ 'Qh', 'Qc' ],
+//   [ 'Qs', 'Qd' ], [ 'Qs', 'Qc' ], [ 'Qd', 'Qc' ]
+// ]
+
+// const { win, loose, tie } = raceRange(combo, range, 1E4)
+// const { winRate, looseRate, tieRate } = rates({ win, loose, tie })
+
+// console.log('JJ performs as follows vs. [ KK, QQ ]')
+// console.log('win: %d%% (%d times)', winRate, win)
+// console.log('loose: %d%% (%d times)', looseRate, loose)
+// console.log('tie: %d%% (%d times)', tieRate, tie)
+
+// prange example
+
+// // const prange = require('./')
+
+// const r1 = prange('AKs-ATs, QQ+')
+// const r2 = prange('JTs-54s')
+
+// console.log(r1)
+// // [ 'AA', 'AKs', 'AQs', 'AJs', 'ATs', 'KK', 'QQ' ]
+
+// console.log(r2)
+// // [ 'JTs', 'T9s', '98s', '87s', '76s', '65s', '54s' ]
+
+// console.log(prange.reverse(r1))
+// // QQ+, ATs+
+
+// console.log(prange.reverse(r2))
+// // JTs-54s
 
 
 
 ReactDOM.render(
-  <P1RangeForm />,
+  <EquityCalculator />,
   document.getElementById('root')
 );
